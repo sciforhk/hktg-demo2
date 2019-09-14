a5ea4a4a4880f2076a186650d6d87307d3138f1a
Yuk Chun Chan <ycchan@link.cuhk.edu.hk>
2019-09-14 13:10:25 -0700

    Updates

---
+++ precache-manifest.016857045f8e0dcf513455a70424108c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8feacbc4601e3cdffd8538399af59860",
+    "url": "/hktg-demo2/index.html"
+  },
+  {
+    "revision": "135395f14f4eec721a0e",
+    "url": "/hktg-demo2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9d063379139f56017aa0",
+    "url": "/hktg-demo2/static/js/2.589a7e48.chunk.js"
+  },
+  {
+    "revision": "135395f14f4eec721a0e",
+    "url": "/hktg-demo2/static/js/main.5b7f92f9.chunk.js"
+  },
+  {
+    "revision": "5bb7ef4977468f7df6e8",
+    "url": "/hktg-demo2/static/js/runtime~main.1efd7900.js"
+  }
+]);
